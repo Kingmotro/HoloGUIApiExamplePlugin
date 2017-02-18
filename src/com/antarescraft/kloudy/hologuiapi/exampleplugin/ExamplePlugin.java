71860e296500b4bf20fc7613df13cfb9928b8059
Kloudy <kloudy@antarescraft.com>
2017-02-18 11:13:14 -0700

    updated to for HoloGUIApiv1.0.9

--- src/com/antarescraft/kloudy/hologuiapi/exampleplugin/ExamplePlugin.java
+++ src/com/antarescraft/kloudy/hologuiapi/exampleplugin/ExamplePlugin.java
@@ -9,7 +9,7 @@ public class ExamplePlugin extends HoloGUIPlugin
 	@Override
 	public void onEnable()
 	{
-		setMinSupportedApiVersion("1.0.8");
+		setMinSupportedApiVersion("1.0.9");
 		checkMinApiVersion();
 		
 		saveDefaultConfig();
